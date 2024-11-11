a2daa6734eed4affd3ceb9ee5a3822f604efa04b
apiseriestech <series.techmov@gmail.com>
2024-11-11 14:39:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -20,3 +20,26 @@ async function fetchData() {
 
 // Call the fetch function to retrieve data from the API
 fetchData();
+
+// Handle dropdown toggle
+const dropdownToggle = document.querySelector('.dropdown-toggle');
+const dropdownContent = document.querySelector('.dropdown-content');
+const arrowIcon = document.querySelector('.arrow');
+
+// Toggle the dropdown menu and the arrow icon
+dropdownToggle.addEventListener('click', () => {
+  dropdownContent.classList.toggle('show');
+  
+  // Toggle the arrow icon
+  if (dropdownContent.classList.contains('show')) {
+    arrowIcon.classList.remove('fa-arrow-down');
+    arrowIcon.classList.add('fa-arrow-up');
+  } else {
+    arrowIcon.classList.remove('fa-arrow-up');
+    arrowIcon.classList.add('fa-arrow-down');
+  }
+});
+
+
+
+
